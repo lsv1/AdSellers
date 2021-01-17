92d230a31091838d2913f441e3d1c3b5292c6c24
lsv1 <luissastreverzun@gmail.com>
2021-01-17 16:21:29 -0500

    Extended load_database.py and added some arguments to main.py.

--- functions/load_database.py
+++ functions/load_database.py
@@ -4,17 +4,19 @@ import logging
 import os
 
 import pandas as pd
+import sqlalchemy
 from tqdm import tqdm
 
 import settings
 
 
-def refresh_adstxt_database():
+def refresh_adstxt_database(filename=None):
     logging.info("Starting db refresh.")
     # Delete existing sqlite db for new pull.
     try:
         os.remove(settings.DB_ADS_TXT)
-    except:
+    except PermissionError:
+        logging.critical("Database in use.")
         pass
 
     date = pd.to_datetime('today').strftime("%Y_%m_%d")
@@ -23,13 +25,24 @@ def refresh_adstxt_database():
     files_to_load = [f for f in os.listdir(settings.DIR_ARCHIVE) if
                      os.path.isfile(os.path.join(settings.DIR_ARCHIVE, f))]
 
+    if filename:
+        files_to_load = [filename]
+
     for file in tqdm(files_to_load):
         file_path = settings.DIR_ARCHIVE + "/" + file
         if date in file and os.stat(file_path).st_size > 0:
-            df = pd.read_csv(file_path)
-            df.to_sql('ads_txt', con=settings.CON_ADS_TXT, if_exists='append')
+            df = pd.read_csv(file_path, parse_dates=['scrape_date'])
+            df.drop_duplicates(subset=['domain', 'ssp', 'account_id', 'type'],
+                               keep='first',
+                               inplace=True)
+            df.to_sql('ads_txt',
+                      con=settings.CON_ADS_TXT,
+                      if_exists='append',
+                      dtype={'scrape_date': sqlalchemy.types.DATE},
+                      index=False)
     logging.info("Finished db refresh.")
 
 
 if __name__ == "__main__":
     refresh_adstxt_database()
+    # refresh_adstxt_database(filename='2021_01_17_01023295056.com.csv')
--- main.py
+++ main.py
@@ -3,10 +3,19 @@
 
 import logging
 
-from functions import ads_txt
+from functions import ads_txt, sellers_json
 
 logging.basicConfig(level=logging.INFO)
 
+
+def main(parse_sellers_json=None, parse_ads_txt=None, load_database=None):
+    if parse_sellers_json:
+        sellers_json.get_all_sellers_json()
+    if parse_ads_txt:
+        ads_txt.async_process()
+    if load_database:
+        load_database.refresh_adstxt_database()
+
+
 if __name__ == "__main__":
-    # sellers_json.get_all_sellers_json()
-    ads_txt.async_process()
+    main(parse_ads_txt=True)
