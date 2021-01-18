b6846af84ea24e4c69eb14b34f82c27767fc4fdd
lsv1 <luissastreverzun@gmail.com>
2021-01-17 20:16:42 -0500

    Remove duplicates in sellers.json list, and clean it up. Refactor some code for getting the list of urls to scrape.

--- functions/sellers_json.py
+++ functions/sellers_json.py
@@ -4,6 +4,8 @@
 import json
 import logging
 import os
+import random
+from urllib.parse import urlparse
 
 import pandas as pd
 import requests
@@ -29,15 +31,27 @@ def get_sellers_json(url):
         logging.debug("Unable to parse JSON " + url)
         return
 
+    # Fix for Telaria's sellers.json because they don't know how specs work.
+    try:
+        data['contact_email'] = data.pop('contactEmail')
+    except:
+        pass
+    try:
+        data['contact_address'] = data.pop('contactAddress')
+    except:
+        pass
+
     logging.debug('Downloaded sellers.json for ' + str(url))
 
     df = pd.json_normalize(data["sellers"])
+
     logging.debug('Normalized sellers.json for ' + str(url))
 
     try:
-        df['seller_domain'] = data['contact_email'].split("@")[1]
+        df['contact_domain'] = data['contact_email'].split("@")[1]
     except:
-        df['seller_domain'] = data['contact_email']
+        df['contact_domain'] = urlparse(url).netloc
+        logging.debug('No sellers.json contact_email for ' + str(url))
 
     df['scrape_date'] = date
 
@@ -56,6 +70,12 @@ def get_sellers_json(url):
         pass
 
     try:
+        df['contact_address'] = data['contact_address']
+    except:
+        logging.debug('No sellers.json contact_address for ' + str(url))
+        pass
+
+    try:
         for identifier in data['identifiers']:
             df[identifier['name']] = identifier['value']
     except:
@@ -66,6 +86,28 @@ def get_sellers_json(url):
     return df
 
 
+def sellers_json_url_list(shuffle=None):
+    with open(settings.FILE_SELLERS_JSON) as f:
+        seller_json_urls = [line.rstrip() for line in f]
+
+        raw_domains = []
+        for url in seller_json_urls:
+            url = url.split("//")[1]
+            url = url.split("/sellers.json")[0]
+            raw_domains.append(url)
+        raw_domains = list(dict.fromkeys(raw_domains))
+
+        clean_urls = []
+        for url in raw_domains:
+            url = "http://" + url + "/sellers.json"
+            clean_urls.append(url)
+
+        if shuffle:
+            random.shuffle(clean_urls)
+
+        return clean_urls
+
+
 def get_all_sellers_json():
     # Create necessary folder if does not exist.
     try:
@@ -73,23 +115,20 @@ def get_all_sellers_json():
     except:
         os.mkdir(settings.DIR_DATABASES)
 
-    with open(settings.FILE_SELLERS_JSON) as f:
-        seller_json_urls = [line.rstrip() for line in f]
-
-        df = pd.DataFrame()
-        for url in seller_json_urls:
-            logging.info(url + " starting processing.")
-            df_sellers = get_sellers_json(url)
-            df = pd.concat([df, df_sellers], ignore_index=True)
-            logging.info(url + " done processing.")
-        df.to_sql(name="sellers",
-                  con=settings.CON_SELLERS_JSON,
-                  if_exists='replace',
-                  dtype={'scrape_date': sqlalchemy.types.DATE})
+    df = pd.DataFrame()
+    for url in sellers_json_url_list(shuffle=True):
+        logging.info(url + " starting processing.")
+        df_sellers = get_sellers_json(url)
+        df = pd.concat([df, df_sellers], ignore_index=True)
+        logging.info(url + " done processing.")
+    df.to_sql(name="sellers",
+              con=settings.CON_SELLERS_JSON,
+              if_exists='replace',
+              dtype={'scrape_date': sqlalchemy.types.DATE})
 
     logging.info('Wrote all sellers.json to database.')
 
 
 if __name__ == "__main__":
     get_all_sellers_json()
-    # get_sellers_json('http://www.justpremium.com/sellers.json')
+    # print(get_sellers_json('https://tremorhub.com/sellers.json'))
